9d9f872c391620d4f1d23cd0565dbce11924f923
JopaBurunduka <127425061+JopaBurunduka@users.noreply.github.com>
2025-05-27 11:53:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,16 @@ def init_db():
         CREATE TABLE IF NOT EXISTS scores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             player_id TEXT,
+            username TEXT,
             score INTEGER
         )
     ''')
     conn.commit()
     conn.close()
 
+# Создаем базу при запуске
 init_db()
+
 
 @app.route('/')
 def home():
@@ -41,10 +44,15 @@ def get_scores():
     c.execute('SELECT * FROM scores ORDER BY id DESC LIMIT 10')
     results = c.fetchall()
     conn.close()
-    
+
     return {
         'scores': [
-            {'id': row[0], 'player_id': row[1], 'score': row[2]}
+            {
+                'id': row[0],
+                'player_id': row[1],
+                'username': row[2],
+                'score': row[3]
+            }
             for row in results
         ]
     }
@@ -53,26 +61,21 @@ def get_scores():
 @app.route('/submit_score', methods=['POST'])
 def submit_score():
     data = request.get_json()
-    if not data or 'player_id' not in data or 'score' not in data:
-        return jsonify({"error": "Missing player_id or score"}), 400
+    if not data or 'player_id' not in data or 'username' not in data or 'score' not in data:
+        return jsonify({"error": "Missing player_id, username or score"}), 400
 
     player_id = data['player_id']
+    username = data['username']
     score = data['score']
 
     conn = sqlite3.connect('scores.db')
     c = conn.cursor()
-    c.execute('INSERT INTO scores (player_id, score) VALUES (?, ?)', (player_id, score))
+    c.execute('INSERT INTO scores (player_id, username, score) VALUES (?, ?, ?)', (player_id, username, score))
     conn.commit()
     conn.close()
 
     return jsonify({"message": "Score saved successfully"}), 200
 
 
-
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
-
-
-
