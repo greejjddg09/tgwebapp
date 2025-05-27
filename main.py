c2930f47199a28636f641ab88a87d40fc65d89c8
JopaBurunduka <127425061+JopaBurunduka@users.noreply.github.com>
2025-05-27 12:46:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -68,9 +68,5 @@ def submit_score():
 
     return jsonify({"message": "Score saved successfully"}), 200
 
-
-
-
-
-if name == 'main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
